fbd1ce56b83356383a8c860beb174587aa0a1c4c
andysung <songzongjian0123@163.com>
2024-07-05 16:56:56 +0800

    学习swiftUI 51～58章节

---
+++ SwiftUIStudyFive/SwiftUIStudyFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyFiveApp.swift
+//  SwiftUIStudyFive
+//
+//  Created by andy on 2024/7/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
